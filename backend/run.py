c62871c6f9052bbdf37ebc44a6998d7e70077c91
mayankisthis <dudemax2324@gmail.com>
2026-07-07 19:54:24 +0530

    Fix Render deployment

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,12 @@
+import os
 import uvicorn
 
 from app.config import settings
 
 if __name__ == "__main__":
-  uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=settings.DEBUG)
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8000)),
+        reload=settings.DEBUG,
+    )
